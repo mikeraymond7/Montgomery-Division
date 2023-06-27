845c8808accdd914c6c8b269c6ed4ecd3081403a
Mike <michael.raymond1@comcast.net>
2023-06-27 13:58:00 -0400

    Updated Test.java

--- Test_all.java
+++ Test_all.java
@@ -1,16 +1,43 @@
 import java.math.BigInteger;
-class Test {
+class Test_all {
 		// use numbers from wiki page
 	 public static void main(String[] args) {
 
 		BigInteger x = new BigInteger("3237060713110073007148766886699519604441026697154840321303454275246551388678908931972014115229134636887179609218980194941195591504909210950881523864482831206308773673009960917501977503896521067960576383840675682767922186426197561618380943384761704705816458520363050428875758915410658086075523991239303855219143333896683424206849747865645694948561760353263220580778056593310261927084603141502585928641771167259436037184618573575983511523340639947855803707216654176622128812031049459145511400081473963578867676698200428287937085882522470310920711555402247510310642532098840992381846882467489498721336450133889385773");
 		BigInteger y = new BigInteger("3237060713110073007148766886699519604441026697154840321303454275246551388678908931972014115229134636887179609218980194941195591504909210950881523864482831206308773673009960917501977503896521067960576383840675682767922186426197561618380943384761704705816458520363050428875758915410658086075523991239303855219143333896683424206849747865645694948561760353263220580778056593310261927084603141502585928641771167259436037184618573575983511523340639947855803707216654176622128812031049459145511400081473963578867676698200428287937085882522470310920711955802247560310542132098840992381846882467489498721336450133889385773");
+/*
+		BigInteger x = new BigInteger("15");
+		BigInteger y = new BigInteger("7");
+*/
 		MontFieldElement m1 = new MontFieldElement(x);
 		MontFieldElement m2 = new MontFieldElement(y);
 
 
 		FieldElement f1 = new FieldElement(x);
 		FieldElement f2 = new FieldElement(y);
+		BigInteger real = f1.mul(f2);
+
+		System.out.print(String.format("Expecting (%s * %s) mod %s", x, y, m1.getN()));
+		System.out.println(" = " + real);
+
+		System.out.println("m1 in Mont Form: " + m1.toMont());
+		System.out.println("m2 in Mont Form: " + m2.toMont());
+		
+		MontFieldElement product = new MontFieldElement(m1.mul(m2), true);
+		m1.fromMont();
+		m2.fromMont();		
+		System.out.println("m1 after fromMont = " + m1.getElement());
+
+		System.out.println("Product in Mont Form: " + product.getElement());
+		System.out.println("Actual Montgomery Product: " + product.fromMont());
+		System.out.println("Real Product: " + real);
+		if (product.getElement().compareTo(real) == 0){
+			System.out.println("\nMontgomery and BigInteger values are equivalent");
+		}
+		else{
+			System.out.println("\nMontgomery and BigInteger values are NOT equivalent");
+		}
+		
 		System.out.println("\n\nTesting looping");
 
 		int bound = 1000000;
@@ -24,15 +51,12 @@ class Test {
 		//long endMMul = System.nanoTime();
 		m1.fromMont();
 		long endM = System.nanoTime();
-		float montTime = (float)(endM-startM)/1000000;
-		System.out.println("Montgomery Version complete in " + montTime + "ms");
 
 		long startF = System.nanoTime();
 		for (int i = 0; i < bound; i++){
 			f1.setElement(f1.mul(f2));
 		}	
 		long endF = System.nanoTime();
-		float standTime = (float)(endF-startF)/1000000;
 
 		System.out.println("m1 = " + m1.getElement());
 		System.out.println("f1 = " + f1.getElement());
@@ -42,15 +66,10 @@ class Test {
 		
 		System.out.println("\nRuntime Statistics: ");
 		
-		System.out.println("Total Montgomery Time = " + montTime + "ms");
+		System.out.println("Total Montgomery Time = " + (endM-startM)/1000000 + "ms");
 		//System.out.println("Montgomery Multiplication Time = " + (endMMul-startMMul)/1000000 + "ms");
-		System.out.println("Total Standard BigInteger Time = " + standTime + "ms");
-		if (montTime > standTime) {
-			System.out.println("Montgomery faster by " + (montTime/standTime) + "x");
-		}
-		else{
-			System.out.println("Standard Implementation faster by " + (standTime/montTime) + "x");
-		}
+		System.out.println("Total Standard BigInteger Time = " + (endF-startF)/1000000 + "ms");
+		System.out.println((float)(endM-startM)/(float)(endF-startF) + "x difference");
 
 	}
 }
